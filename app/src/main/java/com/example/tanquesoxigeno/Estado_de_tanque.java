00d6efc670d4c087d7c55b0c91fa021202c161a3
renatojaraor <57976123+renatojaraor@users.noreply.github.com>
2020-12-03 16:07:25 -0500

    Inicio de sesión con Google funcionando

--- app/src/main/java/com/example/tanquesoxigeno/Estado_de_tanque.java
+++ app/src/main/java/com/example/tanquesoxigeno/Estado_de_tanque.java
@@ -2,7 +2,9 @@ package com.example.tanquesoxigeno;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Estado_de_tanque extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Estado_de_tanque extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estado_de_tanque);
+    }
+
+    public void irMenu(View view){
+        Intent menu = new Intent(getBaseContext(),Menu.class);
+        startActivity(menu);
     }
 }
--- app/src/main/java/com/example/tanquesoxigeno/Menu.java
+++ app/src/main/java/com/example/tanquesoxigeno/Menu.java
@@ -2,13 +2,59 @@ package com.example.tanquesoxigeno;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
 
 public class Menu extends AppCompatActivity {
-
+    TextView txt_name;
+    ImageView imv_photo;
+    Button btn_logout;
+    DatabaseReference db_reference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Intent intent = getIntent();
+        HashMap<String, String> info_user = (HashMap<String, String>)intent.getSerializableExtra("info_user");
+
+        txt_name = findViewById(R.id.textUsername);
+        imv_photo = findViewById(R.id.imageFotoPerfil);
+
+        txt_name.setText(info_user.get("user_name"));
+        String photo = info_user.get("user_photo");
+        Picasso.with(getApplicationContext()).load(photo).into(imv_photo);
+
+
     }
+
+    public void irEstado(View view){
+        Intent estado = new Intent(getBaseContext(),Estado_de_tanque.class);
+        startActivity(estado);
+    }
+
+    public void cerrarSesion(View view){
+        FirebaseAuth.getInstance().signOut();
+        finish();
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("msg", "cerrarSesion");
+        startActivity(intent);
+    }
+    public void iniciarBaseDeDatos(){
+        db_reference = FirebaseDatabase.getInstance().getReference().child("Grupo");
+    }
+
+
+
 }
