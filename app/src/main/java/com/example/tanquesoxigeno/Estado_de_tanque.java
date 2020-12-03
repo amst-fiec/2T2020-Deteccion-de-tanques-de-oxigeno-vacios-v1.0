01c897a815f84641cecb94ff6524919c8a0ab331
ppereira123 <52977467+ppereira123@users.noreply.github.com>
2020-12-03 00:03:11 -0500

    aporte

---
+++ app/src/main/java/com/example/tanquesoxigeno/Estado_de_tanque.java
@@ -0,0 +1,14 @@
+package com.example.tanquesoxigeno;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Estado_de_tanque extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estado_de_tanque);
+    }
+}
